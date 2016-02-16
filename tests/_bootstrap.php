e9a4831160751c1b0a579f16dae73b8f66779fd5
Andrii Vasyliev <sol@hiqdev.com>
2016-02-16 15:53:40 +0000

    inited tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This is AHnames realization of HIAM
+ *
+ * @link      https://github.com/ahnames/hiam
+ * @package   hiam
+ * @license   No license
+ * @copyright Copyright (c) 2015-2016, AHnames (https://ahnames.com/)
+ */
+
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
