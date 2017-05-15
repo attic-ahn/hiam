0d4ab9abcdb97c2fcfbf52b52462c99dfc406bd1
Andrii Vasyliev <sol@hiqdev.com>
2017-05-15 18:36:16 +0200

    fixed configs to new style

--- src/config/params.php
+++ src/config/params.php
@@ -9,10 +9,7 @@
  * @copyright Copyright (c) 2015-2016, AHnames (https://ahnames.com/)
  */
 
-return array_merge(
-    require(Yii::getAlias('@vendor/ahnames/yii2-asset-ahnames/src/config/params.php')),
-    [
-        'passwordResetPage' => 'https://ahnames.com/login/remind',
-        'signupPage'        => 'https://ahnames.com/registration',
-    ]
+return [
+    'passwordResetPage' => 'https://ahnames.com/login/remind',
+    'signupPage'        => 'https://ahnames.com/registration',
 );
